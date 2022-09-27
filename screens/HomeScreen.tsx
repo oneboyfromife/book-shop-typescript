8d897c6f4122d1381f6eded6c0065cdef53c9fde
Akin Tunde <owojoriakintunde@gmail.com>
2022-09-27 11:06:02 +0100

    Updated

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <SearchBar />
+    </SafeAreaView>
   );
 };
 
